a6c463d6fab7f9b702ceed5a3637cc02ee04e34b
Sam Keathley <samncollette@gmail.com>
2020-07-07 17:32:29 -0700

    updated welcome page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -19,19 +19,14 @@ function Welcome() {
                 </Row>
                 <Row className="container welcomeBlurb">
                     <h1>Welcome to the Creature Prompt Generator!</h1>
-                    <br/>
-                    <p>The purpose of this generator is to give you a little push when you're in the mood to create, but are finding that inspiration is a little... lacking.</p>
-                    <p>
-                    (<i>Don't worry-- happens to me all the time. Why do you think I made this thing?</i>)</p>
-                    <p>All you need to do is hit the <strong>"GENERATE"</strong> button. Once you do, you'll get 8 items from 8 different categories:</p>
-                    <p>You can combine some, or all, of these items to create a monster, creature, a character... Really, whatever you want, in your prefered medium. I, personally, like to draw my little creatures. Just have fun with it.
-                    </p>
+                    <br />
+                    <p>In the mood to create something? Is inspiration is a little... lacking?</p>
                 </Row>
                 <Row className="container generateSection">
                     <Col>
-                        <p>Are you ready to get started? Then let's go already!</p>
-                        <Link to="/creature">                        
-                            <Button variant="dark" size="lg">Generate</Button>
+                        <p>Let me help you with that.</p>
+                        <Link to="/creature">
+                            <Button variant="dark" size="lg">GO</Button>
                         </Link>
                     </Col>
                 </Row>
